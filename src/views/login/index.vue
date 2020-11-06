c1f4c16c2b11e1b0551583808ac4e2c23b5e3d35
wangyuxuan666 <308871510@qq.com>
2020-11-06 11:34:34 +0800

    登录页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,36 +2,30 @@
   <div class="login">
     <div class="shadow">
       <el-card class="login_card">
-        <h1
-          style="
-            color: #00b4db;
-            font-size: 36px;
-            letter-spacing: 5px;
-            margin-bottom: 50px;
-          "
-        >
-          用户信息收集
-        </h1>
-        <el-form class="login_card_form">
-          <el-form-item>
+        <img src="../../assets/logo.png" alt="" style="width:23%;margin:15px 0 50px 0">
+        <el-form ref="myForm" class="login_card_form" :model="loginform" :rules="loginRules">
+          <el-form-item prop="userName" style="width: 85%;margin-left:7.5%">
             <el-input
-              style="width: 85%"
-              v-model="userName"
+              v-model="loginform.userName"
               placeholder="账号"
               clearable
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password" style="width: 85%;margin-left:7.5%">
             <el-input
-              v-model="password"
-              style="width: 85%"
+              v-model="loginform.password"
               placeholder="密码"
               show-password
               clearable
             ></el-input>
           </el-form-item>
-          <el-form-item>
-            <el-button type="primary" style="width:85%;background-color:#00b4db">登录</el-button>
+          <el-form-item style="width: 85%;margin-left:7.5%">
+            <el-button
+              type="primary"
+              style="width: 100%"
+              @click="submitLogin"
+              >登录</el-button
+            >
           </el-form-item>
         </el-form>
       </el-card>
@@ -43,8 +37,27 @@
 export default {
   data () {
     return {
-      userName: '',
-      password: ''
+      loginform: {
+        userName: '',
+        password: ''
+      },
+      loginRules: {
+        userName: [{ required: true, message: '请输入您的账号' }],
+        password: [{ required: true, message: '请输入您的密码' }]
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myForm.validate(isok => {
+        if (isok) {
+          if (this.loginform.userName === '1234' && this.loginform.password === '1234') {
+            this.$router.push('/home')
+          } else {
+            this.$message('您的账号或密码有误')
+          }
+        }
+      })
     }
   }
 }
@@ -52,29 +65,31 @@ export default {
 
 <style lang="less" scoped>
 .login {
-  background-image: url("http://goss4.cfp.cn/creative/vcg/nowarter800/new/VCG41N1067908128.jpg");
+  background-image: url("http://goss2.cfp.cn/creative/vcg/nowarter800/new/VCG41471892237.jpg");
   background-size: cover;
-  background-position: bottom;
+  background-position:center;
   width: 100%;
   height: 100vh;
   .shadow {
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.4);
+    background-color: rgba(0, 0, 0, 0.2);
     display: flex;
     justify-content: center;
-    padding-top: 170px;
+    padding-top: 140px;
     box-sizing: border-box;
     .login_card {
-      width: 450px;
-      height: 400px;
-      background-color: rgba(255, 255, 255, 0.9);
+      width: 400px;
+      height: 450px;
+      background-color: rgba(255, 255, 255, 0.2);
       text-align: center;
-      .login_card_form{
-          height:200px;
-          display: flex;
-          flex-direction: column;
-          justify-content: space-around;
+      overflow:unset;
+      .login_card_form {
+        height: 230px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-content: center;
       }
     }
   }
